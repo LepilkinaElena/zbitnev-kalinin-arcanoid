9debdd334ead150e6d166c3fa568734a4553eacc
LepilkinaElena <eklepilkina@gmail.com>
2015-04-21 21:19:15 +0300

    Поправила PublichingCollisionManager

--- src/model/collision/PublishingCollisionManager.java
+++ src/model/collision/PublishingCollisionManager.java
@@ -13,6 +13,11 @@ import com.golden.gamedev.object.collision.AdvanceCollisionGroup;
 import com.golden.gamedev.object.collision.CollisionGroup;
 import com.golden.gamedev.object.collision.CollisionShape;
 import java.util.Map;
+import java.util.Set;
+import model.GameField;
+import model.IngameObject;
+import model.interaction.CollisionEvent;
+import model.interaction.CollisionListener;
 
 /**
  * Менеджер столкновений, сообщающий о коллизиях между объектами
@@ -21,81 +26,60 @@ import java.util.Map;
  */
 public class PublishingCollisionManager extends AdvanceCollisionGroup {
 
-	protected HashMap<CollidedObject, ArrayList<CollidedObject>> _storage = new HashMap<>();
-	
-	@Override
-	public void collided(Sprite arg0, Sprite arg1) {
-            Map map = getStorage(); 
-	}
+    private HashMap<IngameObject, ArrayList<IngameObject>> _storage = new HashMap<>();
+    /**
+     * Список слушателей события
+     */
+    private ArrayList<CollisionListener> _collisionListener = new ArrayList<>();
+    private GameField _field;
 
-	@Override
-	public boolean isCollide(Sprite s1, Sprite s2, CollisionShape shape1, CollisionShape shape2) {
-		
-		boolean retval = super.isCollide(s1, s2, shape1, shape2);
-		
-		// Словарь столкновений будет формироваться в процессе детекции коллизий
-		if (retval) {
-		
-			int obj1colside = -1, obj2colside = -1;
-			switch (this.collisionSide) {
-			case CollisionGroup.BOTTOM_TOP_COLLISION:
-				obj1colside = CollidedObject.SIDE_TOP;
-				obj2colside = CollidedObject.SIDE_BOTTOM;
-				break;
-			case CollisionGroup.TOP_BOTTOM_COLLISION:
-				obj1colside = CollidedObject.SIDE_BOTTOM;
-				obj2colside = CollidedObject.SIDE_TOP;
-				break;
-			case CollisionGroup.RIGHT_LEFT_COLLISION:
-				obj1colside = CollidedObject.SIDE_LEFT;
-				obj2colside = CollidedObject.SIDE_RIGHT;
-				break;
-			case CollisionGroup.LEFT_RIGHT_COLLISION:
-				obj1colside = CollidedObject.SIDE_RIGHT;
-				obj2colside = CollidedObject.SIDE_LEFT;
-				break;
-			default:
-				break;
-			}
-			
-			CollidedObject obj1 = new CollidedObject(
-					((PublishingSprite)s1).getObjectView().getIngameObject(), 
-					new Point2D.Float((float)s1.getOldX(), (float)s1.getOldY()),
-					obj1colside, new Rectangle2D.Double(shape1.getX(), 
-													    shape1.getY(), 
-													    shape1.getWidth(), 
-													    shape1.getHeight()));
-			
-			CollidedObject obj2 = new CollidedObject(
-					((PublishingSprite)s2).getObjectView().getIngameObject(), 
-					new Point2D.Float((float)s2.getOldX(), (float)s2.getOldY()),
-					obj2colside, new Rectangle2D.Double(shape2.getX(), 
-						    							shape2.getY(), 
-						    							shape2.getWidth(), 
-						    							shape2.getHeight()));
-			
-			if (!_storage.keySet().contains(obj1)) {
-				_storage.put(obj1, new ArrayList<CollidedObject>());
-			}
-			_storage.get(obj1).add(obj2);
-		}
-		
-		return retval;
-	}
-	
-	/**
-	 * Получить словарь столкновений объектов в текущем кадре. Объекты представлены в виде 
-	 * CollidedObject, содержащих дополнительную информацию о коллизиях
-	 * @return Словарь столкновений.
-	 */
-	public HashMap<CollidedObject, ArrayList<CollidedObject>> getCollidedStorage() {
-		return _storage;
-	}
-	
-	/**
-	 * Очистить словарь столкновений объектов в текущем кадре.
-	 */
-	public void clearCollidedStorage() {
-		_storage.clear();
-	}
+    public PublishingCollisionManager(GameField field) {
+        super();
+        _field = field;
+    }
+
+    @Override
+    public void collided(Sprite arg0, Sprite arg1) {
+        Map map = getStorage();
+        createIngameObjectMap(map);
+        fireIngameObjectCollided();
+    }
+
+    private void createIngameObjectMap(Map storage) {
+        Set keySet = storage.keySet();
+        for (Object keySprite : keySet) {
+            // Нет такого ключа
+            if (_storage.get(keySprite) == null) {
+                Sprite[] valueSprites = (Sprite[]) storage.get(keySprite);
+                ArrayList<IngameObject> list = new ArrayList<>();
+                for (Sprite value : valueSprites) {
+                    // Добавляется найденный в игровом поле объект
+                    list.add(_field.getObject(value.getID()));
+                }
+            }
+        }
+    }
+
+    public HashMap<IngameObject, ArrayList<IngameObject>> getStorage() {
+        return (HashMap<IngameObject, ArrayList<IngameObject>>) _storage.clone();
+    }
+
+        //-------------------------------------
+    /**
+     * Добавление слушателей события о том, что столкнулись игровые объекты
+     *
+     * @param element слушатель
+     */
+    public void addCollisionListener(CollisionListener element) {
+        _collisionListener.add(element);
+    }
+    
+    /**
+     * Испустить событие о том, что столкнулись игровые объекты
+     */
+    private void fireIngameObjectCollided() {
+        for (CollisionListener listener: _collisionListener) {
+            listener.collisionOccured(new CollisionEvent(this, _storage));
+        }
+    }
 }
