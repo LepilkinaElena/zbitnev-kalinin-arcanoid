5a9b6143040c46f47431dedabc26d1dff45c6787
LepilkinaElena <eklepilkina@gmail.com>
2015-04-22 12:17:14 +0300

    Поправлен GameField

--- src/model/GameField.java
+++ src/model/GameField.java
@@ -3,6 +3,7 @@ package model;
 import java.awt.Dimension;
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import model.ball.Ball;
 import model.ball.BallPositionChangedListener;
@@ -12,10 +13,10 @@ import model.ball.BallPositionChangedListener;
  * @author Nikita Kalinin <nixorv@gmail.com>
  *
  */
-public class GameField implements BallPositionChangedListener {
-
-	private ArrayList<IngameObject> _objects;
+public class GameField {
+        private HashMap <Integer, IngameObject> _objects;
 	private Dimension _dimensions;
+	
 	
     /**
      * Инициализирует поле заданного размера.
@@ -23,7 +24,7 @@ public class GameField implements BallPositionChangedListener {
      */
     public GameField(Dimension size) {
     	
-    	_objects = new ArrayList<>();
+    	_objects = new HashMap<>();
     	_dimensions = size;
     }
     
@@ -32,8 +33,7 @@ public class GameField implements BallPositionChangedListener {
 	 * @param object Объект для добавления
 	 */
 	public void addObject(IngameObject object) {
-		
-		_objects.add(object);
+            _objects.put(object.getId(), object);
 	}
 	
 	/**
@@ -41,7 +41,6 @@ public class GameField implements BallPositionChangedListener {
 	 * @param object Объект для удаления
 	 */
 	public void removeObject(IngameObject object) {
-		
 		_objects.remove(object);
 	}
 	
@@ -53,88 +52,8 @@ public class GameField implements BallPositionChangedListener {
 		
 		return _dimensions;
 	}
-
-	/**
-	 * Реализация этого метода отражает мяч от границ поля.
-	 */
-    @Override
-    public void ballPositionChanged(Ball ball) {
         
-        if (ball.getPosition().y < 0) {
-            ball.setPosition(new Point2D.Float(ball.getPosition().x, 0));
-            ball.setSpeed(ball.getSpeed().flipVertical());
+        public IngameObject getObject(int id) {
+            return _objects.get(id);
         }
-        
-        if (ball.getPosition().x < 0 || ball.getPosition().x + ball.getSize().width > _dimensions.width) {
-            if (ball.getPosition().x < 0) {
-                ball.setPosition(new Point2D.Float(0, ball.getPosition().y));
-            } else {
-                ball.setPosition(new Point2D.Float(_dimensions.width - ball.getSize().width, ball.getPosition().y));
-            }
-            ball.setSpeed(ball.getSpeed().flipHorizontal());
-        }
-    }
-    
-    /**
-     * Обработать столкновения
-     * @param storage Словарь столкновений, где ключ - столкнувшийся объект, значение - 
-     * список объектов, с которыми он столкнулся
-     */
-    /*public void collisionOccured(
-			HashMap<CollidedObject, ArrayList<CollidedObject>> storage) {
-		
-    	// Вместо объектов, от которых принимается эффект (активные)
-    	// передаётся их копия до начала обработки вообще всех столкновений
-    	HashMap<CollidedObject, ArrayList<CollidedObject>> storage_copy = deepCopyStorage(storage);
-    	
-    	Iterator<CollidedObject> i, copyi, j, copyj;
-    	i = storage.keySet().iterator();
-    	copyi = storage_copy.keySet().iterator();
-    	
-    	while (i.hasNext() && copyi.hasNext()) {
-    		
-    		CollidedObject obj1 = i.next();
-    		CollidedObject obj1copy = copyi.next();
-    		j = storage.get(obj1).iterator();
-    		copyj = storage_copy.get(obj1copy).iterator();
-    		
-    		while (j.hasNext() && copyj.hasNext()) {
-    			
-    			CollidedObject obj2 = j.next();
-    			CollidedObject obj2copy = copyj.next();
-    			obj1.object().processCollision(obj1, obj2copy);
-    			obj2.object().processCollision(obj2, obj1copy);
-    		}
-    	}
-	}*/
-    
-    /**
-     * Порождает копию словаря коллизии вместе со всеми хранимыми объектами
-     * @param storage Словарь коллизии
-     * @return Копия словаря коллизии
-     */
-    /*private HashMap<CollidedObject, ArrayList<CollidedObject>> deepCopyStorage(
-    		HashMap<CollidedObject, ArrayList<CollidedObject>> storage) {
-    	
-    	HashMap<CollidedObject, ArrayList<CollidedObject>> deepcopy = new HashMap<>();
-    	
-    	try {
-    		
-    		for (CollidedObject key : storage.keySet()) {
-        		
-        		CollidedObject key_copy = (CollidedObject) key.clone();
-        		ArrayList<CollidedObject> values_copy = new ArrayList<>();
-        		for (CollidedObject obj : storage.get(key)) {
-        			values_copy.add((CollidedObject)obj.clone());
-        		}
-        		
-        		deepcopy.put(key_copy, values_copy);
-        	}
-    	}
-    	catch (CloneNotSupportedException exc) {
-    		exc.printStackTrace();
-    	}
-    	
-    	return deepcopy;
-    }*/
 }
