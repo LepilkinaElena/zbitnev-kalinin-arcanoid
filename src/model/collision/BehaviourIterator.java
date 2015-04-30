9fcdd9299976a01ac032c5f9eee8552b195471f1
LepilkinaElena <eklepilkina@gmail.com>
2015-04-30 09:23:49 +0300

    Добавила класс итератора для контейнера с поведениями

---
+++ src/model/collision/BehaviourIterator.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.collision;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+
+/**
+ *
+ * @author Елена
+ */
+public class BehaviourIterator implements Iterator<CollisionBehaviour> {
+    private Iterator<CollisionBehaviour> _iterator;
+    
+    public BehaviourIterator (ArrayList<CollisionBehaviour> list) {
+        _iterator = list.iterator();
+    }
+    
+    @Override
+    public boolean hasNext() {
+        return _iterator.hasNext();
+    }
+
+    @Override
+    public CollisionBehaviour next() {
+        return _iterator.next();
+    }
+    
+    public void remove() {
+        _iterator.remove();
+    }
+}
