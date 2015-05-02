28319ad5b4ece85baaf3968d5950a40eec9b5f95
LepilkinaElena <eklepilkina@gmail.com>
2015-05-02 09:49:05 +0300

    Добавлен псевдо-объект граница

---
+++ src/model/Boundary.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import model.collisionProcessing.IngameObject;
+import service.PublishingSprite;
+
+/**
+ * Класс для границ (сделан, чтобы можно было вызывать invoke для поведений одинаково)
+ * @author Елена
+ */
+public class Boundary extends IngameObject{
+
+    public Boundary(Speed2D.Axis axis) {
+        super();
+        setAxis(axis);
+    }
+}
