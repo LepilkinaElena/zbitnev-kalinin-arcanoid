3e2c71c89ea319f5483a13685ad81f2b54a40601
LepilkinaElena <eklepilkina@gmail.com>
2015-05-02 11:18:21 +0300

    Поправила класс игрока

--- src/model/Player.java
+++ src/model/Player.java
@@ -40,46 +40,24 @@ public class Player {
 	}
 	
 	/**
-	 * Переместить все свои ракетки в указанную позицию по горизонтали
-	 * @param pos Позиция
-	 */
-	public void setPaddlePositionX(int x) {
-		
-	        int actualx;
-	        if (x > _paddle.getField().getSize().width - _paddle.getSize().width) {
-	            actualx = _paddle.getField().getSize().width - _paddle.getSize().width;
-	        } else if (x < 0) {
-	            actualx = 0;
-	        } else {
-	            actualx = x;
-	        }
-	        _paddle.setPosition(new Point2D.Float(actualx, _paddle.getPosition().y));
-	}
-	
-	/**
-	 * Переместить все свои ракетки в указанном направлении.
-	 * Величину сдвига жёстко задана внутри класса.
-	 * @param dir Направление перемещения
-	 */
-	/*public void movePaddles(Direction dir) {
-		
-	    for (Paddle p : _paddles) {
-	        long delta = Math.round(p.getSize().width / 3.0 * 2.0);
-	        delta = dir.equals(Direction.west()) ? -delta : delta;
-            if (p.getPosition().x + p.getSize().width + delta > p.getField().getSize().width) {
-                p.setPosition(new Point2D.Float(p.getField().getSize().width - p.getSize().width, p.getPosition().y));
-            } else if (p.getPosition().x + delta < 0) {
-                p.setPosition(new Point2D.Float(0, p.getPosition().y));
-            } else {
-                p.move(new Point2D.Float(delta, 0));
-            }
-        }
-	}*/
-	
-	/**
 	 * Заставляет подконтрольные ракетки задать скорость мячам.
 	 */
 	public void pushBalls() {
 	     _paddle.pushBalls();
 	}
+
+    void startAttempt() {
+        _paddle.pushBalls();
+    }
+
+    void processAction(Speed2D speed2D) {
+        _paddle.setSpeed(speed2D);
+    }
+
+    void processAction(Direction direction) {
+        long delta = Math.round(_paddle.getSize().width / 3.0 * 2.0);
+	delta = direction.equals(Direction.west()) ? -delta : delta;
+
+        _paddle.setPosition(new Point2D.Float((float)(_paddle.getPosition().getX() + delta), (float) _paddle.getPosition().getY()));
+    }
 }
