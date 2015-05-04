ccc7b50a2fa6ba904c29030d0d1fcd55cbd6bcd1
KalinkinaMaria <mashyfycya@gmail.com>
2015-05-04 21:58:00 +0300

    Добавлен класс события "Мяч за границей" и интерфейс слушателя этого события

---
+++ src/model/interaction/BallFailListener.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.interaction;
+
+/**
+ *
+ * @author Мария
+ */
+public interface BallFailListener {
+
+    public void endGame(BallFailEvent ballFailEvent);
+    
+}
